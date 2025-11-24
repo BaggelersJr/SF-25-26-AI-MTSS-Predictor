40c6dba4105fdd37cef4128733bf2ebc18c383b3
Luke Tawfik <luketawfik23@gmail.com>
2025-11-24 15:28:14 -0500

    start

---
+++ mtss-predictor.py
@@ -0,0 +1,5 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
