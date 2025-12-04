3ed28aba72b79bba5add750853e29b926e6d56df
Luke Tawfik <luketawfik23@gmail.com>
2025-12-04 00:11:18 -0500

    datasets

--- dataset.py
+++ dataset.py
@@ -3,9 +3,10 @@ import pandas as pd
 np.random.seed(23)
 N = 200
 sex = np.random.choice([0, 1], size=N, p=[0.50, 0.50])
-acwr = np.random.normal(1.1, 0.25, N)
+# 0 = female, 1 = male
+acwr = np.random.normal(1.1, 0.25, N) #Keep unrounded for more variability
 acwr = np.clip(acwr, 0.6, 2.0)
-bmi = np.random.normal(20.5, 2.0, N)
+bmi = np.random.normal(20.5, 2.0, N) #Keep unrounded for more variability
 bmi = np.clip(bmi, 16, 28)  
 history = np.random.choice([0, 1], size=N, p=[0.75, 0.25])
 rest_days = np.random.choice([0, 1, 2, 3, 4, 5], size=N, p=[.40, .35, .15, .05, .03, .02])
