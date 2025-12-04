70205b3ea24950d76c4f04cc9632d4a0b35a3403
Luke Tawfik <luketawfik23@gmail.com>
2025-12-04 00:05:46 -0500

    import to csv

--- dataset.py
+++ dataset.py
@@ -11,3 +11,27 @@ history = np.random.choice([0, 1], size=N, p=[0.75, 0.25])
 rest_days = np.random.choice([0, 1, 2, 3, 4, 5], size=N, p=[.40, .35, .15, .05, .03, .02])
 mileage = np.random.normal(30,7,N)
 mileage = np.clip(mileage, 10, 60)
+logits = (
+        1.3 * (acwr - 1.0) +
+        .15 * (bmi - 20) +
+        .04 * mileage +
+        1.7 * history +
+        .5 * (1 - sex) +
+        (-.6 * rest_days) -
+        3.0
+)
+prob = 1/(1 + np.exp(-logits))
+outcome = np.random.binomial(1, prob)
+
+df = pd.DataFrame({
+    "Sex":sex,
+    "ACWR":acwr,
+    "BMI":bmi,
+    "Injury_History":history,
+    "Rest_Days":rest_days,
+    "Weekly_Mileage":mileage.round(1),
+    "Injury":outcome
+})
+
+df.to_csv("synthetic_MTSS_dataset.csv", index=False)
+df.head()
